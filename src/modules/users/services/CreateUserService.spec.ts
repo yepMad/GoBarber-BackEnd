bff61aa0bb170acc1233e24c2bdbbca121d9f9b7
Eudes Tenório <eudestfilho@gmail.com>
2020-07-27 12:22:53 -0300

    Testes unitários

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('should be able to create a new user', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '1234567',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('should not be able to create a new user with exists email', async () => {
+    const fakeUserRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUserRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '1234567',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'Jane Doe',
+        email: 'johndoe@example.com',
+        password: '7654321',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
